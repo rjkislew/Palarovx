52832c1c91e06b9d12fcceed8033dd490896b677
Joniselury Rasco <joniseluryrasco@gmail.com>
2025-03-07 21:01:53 +0800

    updates

--- Server.Palaro2026/Program.cs
+++ Server.Palaro2026/Program.cs
@@ -7,7 +7,6 @@ using System.Text;
 using Server.Palaro2026;
 using Microsoft.AspNetCore.Http.Json;
 
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
@@ -29,7 +28,7 @@ builder.Services.AddDbContext<Palaro2026Context>(options =>
 // Configure CORS
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowAll", policy => 
+    options.AddPolicy("AllowAll", policy =>
         policy.WithOrigins(
             "https://localhost",
             "https://pgas.ph",
@@ -72,8 +71,7 @@ builder.Services.AddAuthentication(options =>
     options.IncludeErrorDetails = true;
 });
 
-
-builder.Services.AddOpenApi("v1", options => { options.AddDocumentTransformer<BearerSecuritySchemeTransformer>(); });
+builder.Services.AddOpenApi("v1", options => { options.AddDocumentTransformer<BearerSecuritySchemeTransformer>() ; });
 
 var app = builder.Build();
 
@@ -88,14 +86,34 @@ var app = builder.Build();
 //    app.MapOpenApi();
 //}
 
+// Fallback for first-time execution (avoids null values)
+string GetHostAddress = "http://localhost";
+
+app.Use(async (context, next) =>
+{
+    // Get Host and PathBase dynamically
+    var hostAddress = $"{context.Request.Scheme}://{context.Request.Host}";
+    var pathBase = context.Request.PathBase.HasValue ? context.Request.PathBase.Value : "";
+
+    // Update Scalar API Server URL dynamically
+    GetHostAddress = $"{hostAddress}{pathBase}".TrimEnd('/');
+
+    await next();
+});
+
+// Dynamically get Host and PathBase
 app.MapScalarApiReference(options =>
 {
     options.Title = "Palaro 2026 API - Development Stage";
     options.ShowSidebar = true;
     options.HideModels = true;
+
+    // Host & PathBase will be dynamically determined at runtime
+    options.AddServer(GetHostAddress);
 });
 
 app.MapOpenApi();
+
 
 app.MapGet("/api/check", () => "Get Response");
 
