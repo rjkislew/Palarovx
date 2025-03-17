020cf1598c9b73f7f39cefc7ceaca6d3b8b85e92
Joniselury Rasco <joniseluryrasco@gmail.com>
2025-03-17 17:05:36 +0800

    Delete OldClient.Palaro2026 directory

--- OldClient.Palaro2026/Services/APIService.cs
+++
@@ -1,6 +0,0 @@
-﻿public class APIService
-{
-    public static readonly string development_url = "https://localhost:7063/";
-    public static readonly string published_url = "https://pgas.ph/palaro2026API/";
-    public static readonly string Palaro2026API = $"{published_url}api";
-}
