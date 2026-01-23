347ad76b5afc9c0458a1e82b6659ebae1c099413
rjkislew <rjkislew.getes@gmail.com>
2026-01-23 15:45:08 +0800

    Updatre

---
+++ Server.Palaro2026/DTO/ProgressionDTO.cs
@@ -0,0 +1,22 @@
+﻿namespace Server.Palaro2026.DTO
+{
+    public class ProgressionDTO
+    {
+        public class ProgressionTeamSelection
+        {
+            public int? SelectedRegionID { get; set; }
+            public List<string> SelectedPlayerIDs { get; set; } = new();
+            public List<ProfilePlayerDTO> AvailablePlayers { get; set; } = new();
+            public int ExistingVersusTeamID { get; set; }
+            public string? Score { get; set; }
+            public string? Rank { get; set; }
+        }
+
+        public class ProfilePlayerDTO
+        {
+            public string? ID { get; set; }
+            public string? FirstName { get; set; }
+            public string? LastName { get; set; }
+        }
+    }
+}
